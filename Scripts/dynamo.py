a91faaf161569d2ed70d93f2902e34ef3430bc2a
Russell Tan <sell_nat@yahoo.com>
2016-12-11 06:12:20 -0800

    Can query data

--- Scripts/dynamo.py
+++ Scripts/dynamo.py
@@ -2,6 +2,7 @@ from botocore.exceptions import ClientError
 from Scripts import KeyLoader
 import json
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import decimal
 
 # Helper class to convert a DynamoDB item to JSON.
@@ -27,7 +28,7 @@ class dynamoOps(object):
         dynamodb = boto3.resource('dynamodb',aws_access_key_id=id,
             aws_secret_access_key=secret, region_name='us-east-1')
 
-        table = dynamodb.Table('User_Sentiment')
+        table = dynamodb.Table('Raw_Data')
 
 
         return table
@@ -36,29 +37,53 @@ class dynamoOps(object):
 
         table = self.setup()
 
-        try:
-            response = table.get_item(
-                Key={
-                    'User_ID': id
-                }
+        pe = "User_ID, Concept"
+
+
+        response = table.scan(
+            #fe=Key('year').between(1950, 1959);
+            #KeyConditionExpression=Key('TimeStamp').eq('2016')
+            ProjectionExpression=pe
+        )
+        for i in response['Items']:
+            print(json.dumps(i, cls=DecimalEncoder))
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(
+                ProjectionExpression=pe,
+                ExclusiveStartKey=response['LastEvaluatedKey']
             )
-        except ClientError as e:
-            print(e.response['Error']['Message'])
 
-        if 'Item' in response:
+            for i in response['Items']:
+                print(json.dumps(i, cls=DecimalEncoder))
 
-            item = response['Item']
-            print("GetItem succeeded:")
-            print(json.dumps(item, indent=4, cls=DecimalEncoder))
-
-            return item
+            return response['Items']
         else:
             return None
+
+    def addEntry(self, Item):
+        table = self.setup()
+
+        response = table.put_item(Item=Item)
+
+        print(response)
 
 if __name__ == '__main__':
     dym = dynamoOps()
 
     dym.getUserByID('1234')
+
+    Item = {
+        'TimeStamp':'2016_1234',
+        'Concept':'con',
+        'Role':'role',
+        'Score':'1',
+        'Sentiment':'sent',
+        'Text':'text',
+        'User_ID':'1234'
+    }
+
+    dym.addEntry(Item)
 
 
 
