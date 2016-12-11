660a85aadf42de2452782e3bc10b18ea5d6fff08
Russell Tan <sell_nat@yahoo.com>
2016-12-10 19:49:48 -0800

    Fixedadmin output

--- Scripts/Admin_Data.py
+++ Scripts/Admin_Data.py
@@ -6,24 +6,37 @@ class Admin_Data(object):
 
     def getAdminBreakdown(self, area_to_breakdown=None):
         self.results['errors'] = 'ADMIN ERRORS'
-        self.results['cumulative_totals'] = {}
 
         empHealth = []
 
-        empHealth.append('HEALTH SCORE')
+        empHealth.append('HEALTH SCORE1')
+        empHealth.append('HEALTH SCORE2')
+        empHealth.append('HEALTH SCORE3')
+        empHealth.append('HEALTH SCORE4')
 
         empSentiment = []
 
-        empSentiment.append('SENTIMENT SCORE')
+        empSentiment.append('SENTIMENT SCORE1')
+        empSentiment.append('SENTIMENT SCORE2')
+        empSentiment.append('SENTIMENT SCORE3')
+        empSentiment.append('SENTIMENT SCORE4')
 
         empOccurrences = []
 
-        empOccurrences.append('OCCURRERENCE DATA')
+        empOccurrences.append('OCCURRERENCE DATA1')
+        empOccurrences.append('OCCURRERENCE DATA2')
+        empOccurrences.append('OCCURRERENCE DATA3')
+        empOccurrences.append('OCCURRERENCE DATA4')
 
 
-        self.results['cumulative_totals']['cumulEmpHealth'] = empHealth
-        self.results['cumulative_totals']['cumulSentiment'] = empSentiment
-        self.results['cumulative_totals']['cumulOccurances'] = empOccurrences
+        self.results['cumulEmpHealth'] = empHealth
+        self.results['cumulSentiment'] = empSentiment
+        self.results['cumulOccurances'] = empOccurrences
+
+        for item in self.results:
+            print('column ' + item)
+            for d in self.results[item]:
+                print('row ' + d)
 
 
 
